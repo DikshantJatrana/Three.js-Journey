e45d9e10a67fdcdd557d526c1bf5dd833bbec7eb
DikshantJatrana <dikshantjatrana02@gmail.com>
2025-07-04 15:30:57 +0530

    Learn Transform

--- 02Transform/script.js
+++ 02Transform/script.js
@@ -10,8 +10,24 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
-
 scene.add(mesh);
+
+//postions
+mesh.position.set(1, 0.6, -0.9);
+console.log(mesh.position.length());
+
+//scale
+mesh.scale.x = 2;
+mesh.scale.y = 0.5;
+mesh.scale.z = 0.5;
+
+//rotation
+mesh.rotation.y = 0.25;
+mesh.rotation.z = 0.5;
+
+//Axes Helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 //sizes
 const sizes = {
@@ -23,6 +39,12 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
+
+//Look at
+camera.lookAt(mesh.position);
+
+//distance of mesh to camera
+console.log(mesh.position.distanceTo(camera.position));
 
 //render
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
